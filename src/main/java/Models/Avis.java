583f9d2353c979a919c340703f8bd9afcaf87784
hayder <benahmedhayder10@gmail.com>
2026-05-10 21:09:54 +0100

    feature : modif

--- src/main/java/Models/Avis.java
+++ src/main/java/Models/Avis.java
@@ -5,38 +5,123 @@ import java.time.LocalDateTime;
 public class Avis {
     private int idAvis;
     private int idReservation;
-    private int utilisateurId;
+    private int utilisateur_id;
     private int note;
     private String commentaire;
     private LocalDateTime dateAvis;
-    
-    public Avis() {}
-    
-    public Avis(int idAvis, int idReservation, int utilisateurId, int note, String commentaire, LocalDateTime dateAvis) {
+    private float sentiment_score;
+    private float authenticity_score;
+    private String review_category;
+    private int is_verified;
+
+    public Avis(int idAvis, int idReservation, int utilisateur_id, int note, String commentaire, LocalDateTime dateAvis, float sentiment_score, float authenticity_score, String review_category, int is_verified) {
+
         this.idAvis = idAvis;
         this.idReservation = idReservation;
-        this.utilisateurId = utilisateurId;
+        this.utilisateur_id = utilisateur_id;
         this.note = note;
         this.commentaire = commentaire;
         this.dateAvis = dateAvis;
+        this.sentiment_score = sentiment_score;
+        this.authenticity_score = authenticity_score;
+        this.review_category = review_category;
+        this.is_verified = is_verified;
     }
-    
-    // Getters and Setters
-    public int getIdAvis() { return idAvis; }
-    public void setIdAvis(int idAvis) { this.idAvis = idAvis; }
-    
-    public int getIdReservation() { return idReservation; }
-    public void setIdReservation(int idReservation) { this.idReservation = idReservation; }
-    
-    public int getUtilisateurId() { return utilisateurId; }
-    public void setUtilisateurId(int utilisateurId) { this.utilisateurId = utilisateurId; }
-    
-    public int getNote() { return note; }
-    public void setNote(int note) { this.note = note; }
-    
-    public String getCommentaire() { return commentaire; }
-    public void setCommentaire(String commentaire) { this.commentaire = commentaire; }
-    
-    public LocalDateTime getDateAvis() { return dateAvis; }
-    public void setDateAvis(LocalDateTime dateAvis) { this.dateAvis = dateAvis; }
+
+    public int getIdAvis() {
+        return idAvis;
+    }
+
+    public void setIdAvis(int idAvis) {
+        this.idAvis = idAvis;
+    }
+
+    public int getIdReservation() {
+        return idReservation;
+    }
+
+    public void setIdReservation(int idReservation) {
+        this.idReservation = idReservation;
+    }
+
+    public int getUtilisateur_id() {
+        return utilisateur_id;
+    }
+
+    public void setUtilisateur_id(int utilisateur_id) {
+        this.utilisateur_id = utilisateur_id;
+    }
+
+    public int getNote() {
+        return note;
+    }
+
+    public void setNote(int note) {
+        this.note = note;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
+
+    public LocalDateTime getDateAvis() {
+        return dateAvis;
+    }
+
+    public void setDateAvis(LocalDateTime dateAvis) {
+        this.dateAvis = dateAvis;
+    }
+
+    public float getSentiment_score() {
+        return sentiment_score;
+    }
+
+    public void setSentiment_score(float sentiment_score) {
+        this.sentiment_score = sentiment_score;
+    }
+
+    public float getAuthenticity_score() {
+        return authenticity_score;
+    }
+
+    public void setAuthenticity_score(float authenticity_score) {
+        this.authenticity_score = authenticity_score;
+    }
+
+    public String getReview_category() {
+        return review_category;
+    }
+
+    public void setReview_category(String review_category) {
+        this.review_category = review_category;
+    }
+
+    public int getIs_verified() {
+        return is_verified;
+    }
+
+    public void setIs_verified(int is_verified) {
+        this.is_verified = is_verified;
+    }
+
+    @Override
+    public String toString() {
+        return "Avis{" +
+                "idAvis=" + idAvis +
+                ", idReservation=" + idReservation +
+                ", utilisateur_id=" + utilisateur_id +
+                ", note=" + note +
+                ", commentaire='" + commentaire + '\'' +
+                ", dateAvis=" + dateAvis +
+                ", sentiment_score=" + sentiment_score +
+                ", authenticity_score=" + authenticity_score +
+                ", review_category='" + review_category + '\'' +
+                ", is_verified=" + is_verified +
+                '}';
+    }
 }
+
--- src/main/java/Models/Evenement.java
+++ src/main/java/Models/Evenement.java
@@ -9,18 +9,29 @@ public class Evenement
     private String description;
     private LocalDate date;
     private String localisation;
+    private float popularity_score;
+    private int predicted_attendance;
+    private float dynamic_price;
+    private float base_price;
+    private int max_capacity;
+    private String venue_layout;
 
     public Evenement()
     {
     }
 
-    public Evenement(int idEvenement, String titre, String description, LocalDate date, String localisation)
-    {
+    public Evenement(int idEvenement, String titre, String description, LocalDate date, String localisation, float popularity_score, int predicted_attendance, float dynamic_price, float base_price, int max_capacity, String venue_layout) {
         this.idEvenement = idEvenement;
         this.titre = titre;
         this.description = description;
         this.date = date;
         this.localisation = localisation;
+        this.popularity_score = popularity_score;
+        this.predicted_attendance = predicted_attendance;
+        this.dynamic_price = dynamic_price;
+        this.base_price = base_price;
+        this.max_capacity = max_capacity;
+        this.venue_layout = venue_layout;
     }
 
     public int getIdEvenement()
@@ -73,15 +84,68 @@ public class Evenement
         this.localisation = localisation;
     }
 
+    public float getPopularity_score() {
+        return popularity_score;
+    }
+
+    public void setPopularity_score(float popularity_score) {
+        this.popularity_score = popularity_score;
+    }
+
+    public int getPredicted_attendance() {
+        return predicted_attendance;
+    }
+
+    public void setPredicted_attendance(int predicted_attendance) {
+        this.predicted_attendance = predicted_attendance;
+    }
+
+    public float getDynamic_price() {
+        return dynamic_price;
+    }
+
+    public void setDynamic_price(float dynamic_price) {
+        this.dynamic_price = dynamic_price;
+    }
+
+    public float getBase_price() {
+        return base_price;
+    }
+
+    public void setBase_price(float base_price) {
+        this.base_price = base_price;
+    }
+
+    public int getMax_capacity() {
+        return max_capacity;
+    }
+
+    public void setMax_capacity(int max_capacity) {
+        this.max_capacity = max_capacity;
+    }
+
+    public String getVenue_layout() {
+        return venue_layout;
+    }
+
+    public void setVenue_layout(String venue_layout) {
+        this.venue_layout = venue_layout;
+    }
+
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "Evenement{" +
                 "idEvenement=" + idEvenement +
                 ", titre='" + titre + '\'' +
                 ", description='" + description + '\'' +
                 ", date=" + date +
                 ", localisation='" + localisation + '\'' +
+                ", popularity_score=" + popularity_score +
+                ", predicted_attendance=" + predicted_attendance +
+                ", dynamic_price=" + dynamic_price +
+                ", base_price=" + base_price +
+                ", max_capacity=" + max_capacity +
+                ", venue_layout='" + venue_layout + '\'' +
                 '}';
     }
 }
